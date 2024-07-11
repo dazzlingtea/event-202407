233193d24b688e6463be081e106b91fe005dd9a2
dazzlingtea <kitsnr08@gmail.com>
2024-07-11 11:28:39 +0900

    # router 리팩토링 및 홈 children 추가

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -10,51 +10,74 @@ import NewEvent from '../pages/NewEvent';
 import EditPage from '../pages/EditPage';
 import { action as manipulateAction }
   from '../components/EventForm';
+import Events from "../pages/Events";
+import WelcomePage from "../pages/WelcomePage";
+import SignUpPage from "../pages/SignUpPage";
 
 // 라우터 설정
+
+const homeRouter = [
+  {
+    index: true,
+    element: <WelcomePage />
+  }, // 웰컴페이지 (로그인 화면 or 로그인완료화면)
+  {
+    path: 'sign-up',
+    element: <SignUpPage />
+  } // 회원가입 페이지
+];
+
+const eventsRouter = [
+  {
+    index: true,
+    element: <Events />,
+    // loader: eventListLoader,
+  },
+  {
+    path: ':eventId',
+    loader: eventDetailLoader,
+    // element: <EventDetail />,
+    // loader가 children에게 직접적으로 연결되지 않아
+    // EventDetail에서 loader를 사용하지 못하고 있음.
+    id: 'event-detail', // loader에게 ID 부여
+    children: [
+      {
+        index: true,
+        element: <EventDetail />,
+        action: deleteAction
+      },
+      {
+        path: 'edit',
+        element: <EditPage />,
+        action: manipulateAction
+      },
+    ]
+  },
+  {
+    path: 'new',
+    element: <NewEvent />,
+    // 서버에 갱신데이터요청을 보낼 때 트리거
+    action: manipulateAction
+  },
+];
+
+
+
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Home /> },
+      {
+        path: '/',
+        element: <Home />,
+        children: homeRouter
+      },
       {
         path: 'events',
         element: <EventLayout />,
-        children: [
-          {
-            index: true,
-            element: <Events />,
-            // loader: eventListLoader,
-          },
-          {
-            path: ':eventId',
-            loader: eventDetailLoader,
-            // element: <EventDetail />,
-            // loader가 children에게 직접적으로 연결되지 않아
-            // EventDetail에서 loader를 사용하지 못하고 있음.
-            id: 'event-detail', // loader에게 ID 부여
-            children: [
-              {
-                index: true,
-                element: <EventDetail />,
-                action: deleteAction
-              },
-              {
-                path: 'edit',
-                element: <EditPage />,
-                action: manipulateAction
-              },
-            ]
-          },
-          {
-            path: 'new',
-            element: <NewEvent />,
-            // 서버에 갱신데이터요청을 보낼 때 트리거
-            action: manipulateAction
-          },
-        ]
+        children: eventsRouter
       },
     ]
   },
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Outlet} from 'react-router-dom'; // Home 칠드런 렌더링
 
 const Home = () => {
   console.log('Home 실행!');
   return (
     <>
       <h1>My Home Page</h1>
-      <p>
-        <Link to={'events'}>Events</Link>페이지로 이동하기
-      </p>
+      <Outlet />
     </>
   );
 };
