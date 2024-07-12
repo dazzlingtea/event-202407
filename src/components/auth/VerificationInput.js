9812b8911821d4843ce6db5ad52e351661fd2d9a
dazzlingtea <kitsnr08@gmail.com>
2024-07-12 09:34:29 +0900

    # 가입-이메일 인증번호 - 4자리

--- src/components/auth/VerificationInput.js
+++ src/components/auth/VerificationInput.js
@@ -6,11 +6,18 @@ const VerificationInput = () => {
     <>
      <p>Step 2: 이메일로 전송된 인증번호 4자리를 입력해주세요. </p>
       <div className={styles.codeInputContainer}>
-        <input
-          type="text"
-        />
+        {
+          Array.from(new Array(4)).map((_, index)=>(
+            <input
+              key={index}
+              type="text"
+              maxLength={1}
+              className={styles.codeInput}
+            />
+          ))
+        }
 
-      </div> 
+      </div>
     </>
   );
 };
