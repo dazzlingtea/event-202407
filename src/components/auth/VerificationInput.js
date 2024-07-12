9bb233ab54bbae00c198946149ecb888a7ccba63
dazzlingtea <kitsnr08@gmail.com>
2024-07-12 09:59:47 +0900

    # 가입-이메일 인증코드 전송

--- src/components/auth/VerificationInput.js
+++ src/components/auth/VerificationInput.js
@@ -1,10 +1,13 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './SignUpForm.module.scss';
 
 const VerificationInput = () => {
 
   // 여러 개의 컴포넌트에 ref를 거는 방법
   const inputsRef = useRef([]);
+
+  // 입력한 인증코드를 저장
+  const [codes, setCodes] = useState([]);
 
   useEffect(() => {
     // 처음엔 첫번째 칸에 포커싱
@@ -18,9 +21,33 @@ const VerificationInput = () => {
     }
   };
 
-  const changeHandler = (index) => {
+  // 서버에 검증 요청 보내기
+  const verifyCode = async (code) => {
+    console.log('요청전송!: ', code);
+  };
+
+
+  const changeHandler = (index, inputValue) => {
+
+    const updatedCodes = [ ...codes, inputValue ];
+    console.log('updatedCodes: ',updatedCodes);
+
+    // codes 변수에 입력한 숫자 담아놓기
+    setCodes(updatedCodes);
+
     // 입력이 끝나면 다음 칸으로 포커스 이동
     focusNextInput(index);
+
+    // 입력한 숫자 합치기
+    // join() : 배열 안에 있는 요소를 전부 연결
+    if(updatedCodes.length === 4 && index === 4) {
+      const code = updatedCodes.join('');
+      // console.log('배열 join 결과: ', code);
+
+      // 서버로 인증코드 검증 요청 전송
+      verifyCode(code);
+
+    }
   }
 
   return (
@@ -35,7 +62,7 @@ const VerificationInput = () => {
               type="text"
               maxLength={1}
               className={styles.codeInput}
-              onChange={(e) => changeHandler(index+1)}
+              onChange={(e) => changeHandler(index+1, e.target.value)}
             />
           ))
         }
