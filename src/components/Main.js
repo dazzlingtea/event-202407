e38e3630c73668ba3d0d4265ae7d9673c6936499
dazzlingtea <kitsnr08@gmail.com>
2024-07-16 09:57:15 +0900

    # 로그인 성공 시 조건부 렌더링

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <h2>ㅌㅌㅌ님 환영합니다.</h2>
+      <h3>현재 권한: [VIP]</h3>
+      <button>logout</button>
+    </>
+  );
+};
+
+export default Main;
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LoginForm from "../components/auth/LoginForm";
+import Main from "../components/Main";
 
 const WelcomePage = () => {
+  console.log('WelcomePage 실행!')
+  // 로그인 여부
+
+  const [isLoggedIn, setIsLoggedIn] =
+    useState(localStorage.getItem('userData') !== null);
+
+  useEffect(() => {
+    const userData = localStorage.getItem('userData');
+    if(userData) setIsLoggedIn(true);
+    else setIsLoggedIn(false);
+  }, [isLoggedIn]);
+
   return (
-    <LoginForm />
+    <>
+      {!isLoggedIn && <LoginForm/>}
+      {isLoggedIn && <Main/>}
+    </>
   );
 };
 
